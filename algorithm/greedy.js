2a73556b3382c4bdf2403932b41da519539cc1cb
jjuny0113 <jjuny920113@gmail.com>
2021-03-24 22:15:51 +0900

    [ADD] 분할가능 배낭문제

--- algorithm/greedy.js
+++ algorithm/greedy.js
@@ -26,3 +26,30 @@ const activitySelection = (act) => {
 };
 
 activitySelection(activity);
+
+
+const text = [[1,60,10],[2,100,20],[3,120,30]]
+
+function fractionalKnapsack(item,w){
+  const sorted = item.sort((prev,cur)=> cur[1]/cur[2]- prev[1]/prev[2])
+  console.log(sorted)
+  let result = 0
+  let limit = w
+  for(let i = 0; i<sorted.length;i++){
+    let cur = sorted[i]
+    if(limit > 0 ){
+      if(limit>cur[2]){
+      limit -= cur[2]
+      result += cur[1]
+        } else{
+          result += cur[1]/cur[2] * limit
+          limit = 0
+        }
+    }else{
+      break
+    }
+  }
+  return result
+}
+
+fractionalKnapsack(text,50)
