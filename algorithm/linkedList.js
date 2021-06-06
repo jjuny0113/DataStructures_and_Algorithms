6182d1f09716efc0663a6f57ff07f6846832722a
jjuny0113 <68732435+jjuny0113@users.noreply.github.com>
2021-06-06 19:34:20 +0900

    [ADD] js linkedList 추가

---
+++ algorithm/linkedList.js
@@ -0,0 +1,134 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+    this.size = 0;
+  }
+
+  add(element) {
+    let node = new Node(element);
+    let current;
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  insertAt(element, index) {
+    if (index < 0 || index > this.size) {
+      return console.log("잘못입력하셨습니다.");
+    } else {
+      const node = new Node(element);
+      let curr, prev;
+
+      if (index === 0) {
+        node.next = this.head;
+        this.haed = node;
+      } else {
+        curr = this.head;
+        let it = 0;
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+        node.next = curr;
+        prev.next = node;
+      }
+    }
+    this.size++;
+  }
+
+  removeidx(index) {
+    if (index < 0 || index >= this.size) {
+      return console.log("잘못입력했습니다.");
+    } else {
+      let curr,
+        prev,
+        it = 0;
+      curr = this.head;
+      prev = curr;
+
+      if (index === 0) {
+        this.head = curr.next;
+      } else {
+        while (it < index) {
+          it++;
+          prev = curr;
+          curr = curr.next;
+        }
+        prev.next = curr.next;
+      }
+      this.size--;
+      return curr.value;
+    }
+  }
+
+  removeValue(value) {
+    let curr = this.head;
+    let prev = null;
+    while (curr !== null) {
+      if (curr.value === value) {
+        if (prev === null) {
+          this.head === curr.next;
+        } else {
+          prev.next = curr.next;
+        }
+        this.size--;
+        return curr.value;
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+    return -1;
+  }
+
+  indexOf(value) {
+    let count = 0;
+    let curr = this.head;
+
+    while (curr !== null) {
+      if ((curr.value = value)) {
+        return count;
+      }
+      count++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  sizeOfList() {
+    console.log(this.size);
+  }
+
+  printList() {
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+}
+
+const list = new LinkedList();
+list.add(3);
+list.add(5);
+list.insertAt(11, 1);
